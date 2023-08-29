549322854a51bd4ef1a9e2fc972e96f21a1eba2e
HoangAnh <hoanganh51199@gmail.com>
2023-08-29 17:04:57 +0700

    final lan 5

--- src/store/reducer/datGheReducer.js
+++ src/store/reducer/datGheReducer.js
@@ -56,30 +56,59 @@ export const datGheReducer = (state = DEFAULT_STATE, action) => {
 
       const idxHang = data.findIndex(// tim hàng ?
       (element) => element.hang === cartList.hang //tìm index theo payload soGhe
-
     );
-     //cartList: [element:{},hang] 2 propery
-     
-     if (idxHang !== -1) {
-      //  console.log(data);
-      const indexSG = data[idxHang].danhSachGhe.findIndex(//tim soghe
-        (element) => element.soGhe === cartList[idxHang].element.soGhe //tìm index theo payload soGhe
-
-      );
-      //  console.log(index);
-      if (indexSG !== -1 && data[idxHang].danhSachGhe[indexSG].daDat == false) {
-        data[idxHang].danhSachGhe[indexSG].daDat = !data[idxHang].danhSachGhe[indexSG].daDat;
-
-      } 
 
 
+    console.log(data);
+    console.log(cartList);
+
+     console.log(idxHang);
+
+     for (const [index1, property1] of data.entries()) {
+      for (const [index2, property2] of cartList.entries()) {
+        if (property1.hang === property2.hang) {
+          const hangData =data[index1].danhSachGhe;
+          const hangCart = cartList[index2].element
+          console.log(`Vị trí của thuộc tính có giá trị giống nhau: ${index1} trong mảng 1 và ${index2} trong mảng 2`);
+         console.log(hangData);
+         console.log(hangCart);
+         
+         const idxSoGhe = hangData.findIndex(//tim soghe
+         (element) => element.soGhe === hangCart.soGhe//tìm index theo payload soGhe
+          
+       );
+       console.log(idxSoGhe);
+       console.log(hangData[idxSoGhe]);
+
+       if(idxSoGhe !== -1 && hangData[idxSoGhe].daDat==false) {
+        hangData[idxSoGhe].daDat = !hangData[idxSoGhe].daDat ;
+          }
+          
+        
+          
+      }
     }
+  }
+      //cartList: [element:{},hang] 2 propery
+
+    //  if (idxHang !== -1) {
+    //   //  console.log(data);
+    //   const indexSG = data[idxHang].danhSachGhe.findIndex(//tim soghe
+    //     (element) => element.soGhe === cartList[idxHang].element.soGhe //tìm index theo payload soGhe
+
+    //   );
+    //    console.log(indexSG);
+    //   if (indexSG !== -1 && data[idxHang].danhSachGhe[indexSG].daDat == false) {
+    //     data[idxHang].danhSachGhe[indexSG].daDat = !data[idxHang].danhSachGhe[indexSG].daDat;
+
+    //   } 
+    // }
     
    
     
        cartList.splice(0, cartList.length);
-      state.cartList = cartList; //render lại
-      state.danhSachGhe = data;//render lại ra table -> daDat
+       state.cartList = cartList; //render lại
+       state.danhSachGhe = data;//render lại ra table -> daDat
       break;
     }
 
